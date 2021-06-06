533ed494664d8bd22bd6773372d54e91651ef12e
wogh002 <ekekekem159@gmail.com>
2021-06-06 11:43:00 +0900

    make search component

--- src/app.jsx
+++ src/app.jsx
@@ -1,9 +1,23 @@
 import { useEffect, useState } from 'react';
-import './app.css';
+import styles from './app.module.css';
 import VideoList from './components/video_list/video_list';
-
+import SearchHeader from './components/search_header/search_header';
 function App() {
   const [videos, setVideos] = useState([]);
+
+  const onSearch = query => {
+    const requestOptions = {
+      method: 'GET',
+      redirect: 'follow'
+    };
+    fetch(`https://youtube.googleapis.com/youtube/v3/search?key=AIzaSyCYNqNYWWu8JXtSK1I62-LbQa6oxA4WDxE&part=snippet&type=video&maxResults=25&q=${query}&key=AIzaSyCYNqNYWWu8JXtSK1I62-LbQa6oxA4WDxE`
+      , requestOptions)
+      .then(response => response.json())
+      .then(result => result.items.map(item => ({ ...item, id: item.id.videoId })))
+      .then(items => setVideos(items))
+      .catch(error => console.log('error', error));
+  }
+
   useEffect(() => {
     const requestOptions = {
       method: 'GET',
@@ -17,7 +31,11 @@ function App() {
   }, []);
 
   return (
-    <VideoList videos={videos} />
+    <div className={styles.app}>
+      <SearchHeader onSearch={onSearch} />
+      <VideoList videos={videos} />
+    </div>
+
   )
 }
 
---
+++ src/components/search_header/search_header.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import styles from './search_header.module.css';
+
+// /1. 리액트에서 빌드가 된 최종 배포상태에서 동작하는 경로를 기준으로 설정이 되기 때문에 /public/.. 을 붙여주지 않아도 되어요 :)
+// 배포되면 public/안에 있는 index.html 등 리소스들이 최상위 루트로 지정되거든요
+
+const SearchHeader = ({ onSearch }) => {
+    const inputRef = React.useRef();
+    const handleSearch = inputValue => {
+        inputRef.current.value = '';
+        onSearch(inputValue);
+    }
+    const onKeyPress = event => {
+        if (event.key === 'Enter') {
+            handleSearch(inputRef.current.value)
+        }
+    }
+    const onClick = () => {
+        handleSearch(inputRef.current.value)
+    }
+
+
+    return (
+        <header className={styles.header}>
+            <a href="./" className={styles.logo}>
+                <img className={styles.img} src='./images/logo.png' alt="logo" ></img>
+                <h1 className={styles.title}>Youtube</h1>
+            </a>
+            <input className={styles.input} ref={inputRef} onKeyPress={onKeyPress} type="text" placeholder="Search..." />
+            <button className={styles.button} type="submit" onClick={onClick}>
+                <img className={styles.buttonImg} src="./images/search.png" alert="search"></img>
+            </button>
+        </header>
+    )
+};
+
+export default SearchHeader;
